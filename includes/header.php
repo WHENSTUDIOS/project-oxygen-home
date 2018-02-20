978e75bdd1f06e2442fe7dc523d7f201ab93040a
chrissKLO <real.chriss.klosowski@gmail.com>
2018-02-20 20:10:36 +0800

    Initial Commit
    Initial git commit

---
+++ includes/header.php
@@ -0,0 +1,166 @@
+<header id="header">
+				<div class="container">
+					<div class="row primary-header">
+						<a href="index.php" class="col-xs-12 col-sm-2 brand" title="Welcome to VATFRANCE"><img src="assets/img/VATFRANCE.PNG" alt="VATFRANCE"></a>
+						<div class="social-links col-xs-12 col-sm-10">
+							<a href="volunteer.php" class="btn btn-default btn-volunteer">User Login</a>
+							<ul class="social-icons hidden-xs">
+								<li>
+									<a href="" target="_blank">FR <i class="fa fa-flag"></i></a>
+								</li>
+								<li>
+									<a href="" target="_blank">EN <i class="fa fa-flag"></i></a>
+								</li>
+
+							</ul>
+						</div>
+					</div>
+				</div>
+				<div class="navbar navbar-default" role="navigation">
+					<div class="container">
+						<!-- Brand and toggle get grouped for better mobile display -->
+						<div class="navbar-header">
+							<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+								<span class="sr-only">Toggle navigation</span>
+								<span class="icon-bar"></span>
+								<span class="icon-bar"></span>
+								<span class="icon-bar"></span>
+							</button>
+						</div>
+
+						<!-- Collect the nav links, forms, and other content for toggling -->
+						<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+							<nav>
+							<ul class="nav navbar-nav">
+								<li class="active">
+									<a href="javascript:;"  data-toggle="dropdown"  class="submenu-icon">Home <span class="glyphicon glyphicon-chevron-down"></span> <span class="glyphicon glyphicon-chevron-up"></span> </a>
+									<div  class="dropdown-menu">
+										<ul>
+											<li>
+												<a href="">About Us</a>
+											</li>
+											<li>
+												<a href="">Join VATSIM</a>
+											</li>
+											<li>
+												<a href="">Join VATFRANCE</a>
+											</li>
+											<li>
+												<a href="">Staff Members</a>
+											</li>
+											<li>
+												<a href="">Contact Us</a>
+											</li>
+											
+										</ul>
+									</div>
+								</li>
+
+								<li>
+									<a href="javascript:;"  data-toggle="dropdown"  class="submenu-icon">ATC  <span class="glyphicon glyphicon-chevron-down"></span> <span class="glyphicon glyphicon-chevron-up"></span> </a>
+									<!-- Drop Down  -->
+
+									<div  class="dropdown-menu">
+										<ul>
+											<li>
+												<a href="">Getting Started</a>
+											</li>
+											<li>
+												<a href="">Approved Controllers</a>
+											</li>
+											<li>
+												<a href="">Approved Visitors</a>
+											</li>
+											<li>
+												<a href="">Solo Validations</a>
+											</li>
+											<li>
+												<a href="">Controller Files</a>
+											</li>
+										</ul>
+									</div>
+								</li>
+								<li>
+									<a href="javascript:;" data-toggle="dropdown" class="submenu-icon">Pilots <span class="glyphicon glyphicon-chevron-down"></span> <span class="glyphicon glyphicon-chevron-up"></span> </a>
+									<!-- Drop Down  -->
+									<div  class="dropdown-menu">
+										<ul>
+											<li>
+												<a href="">Chart System</a>
+											</li>
+											<li>
+												<a href="">Pilot Ratings</a>
+											</li>
+											<li>
+												<a href="">Pilot Resources</a>
+											</li>
+											<li>
+												<a href="">VATFRANCE PTD</a>
+											</li>
+										</ul>
+									</div>
+									<!-- end  -->
+
+								</li>
+								<li>
+									<a href="javascript:;" data-toggle="dropdown" class="submenu-icon">Events <span class="glyphicon glyphicon-chevron-down"></span> <span class="glyphicon glyphicon-chevron-up"></span> </a>
+										<div  class="dropdown-menu">
+										<ul>
+										<li>
+												<a href="">Event Calendar</a>
+											</li>
+											<li>
+												<a href="">Suggest an event</a>
+											</li>
+										</ul>
+
+									</div>
+								</li>
+								
+								<li>
+									<a href="javascript:;" data-toggle="dropdown" class="submenu-icon">Members <span class="glyphicon glyphicon-chevron-down"></span> <span class="glyphicon glyphicon-chevron-up"></span> </a>
+									<!-- Drop Down  -->
+									<div  class="dropdown-menu">
+										<ul>
+											<li>
+												<a href="">vACC Stats</a>
+											</li>
+											<li>
+												<a href="">Booking System</a>
+											</li>
+
+										</ul>
+									</div>
+								</li>
+								<li>
+									<a href="javascript:;" data-toggle="dropdown" class="submenu-icon">FIRs <span class="glyphicon glyphicon-chevron-down"></span> <span class="glyphicon glyphicon-chevron-up"></span> </a>
+									<!-- Drop Down  -->
+									<div  class="dropdown-menu">
+										<ul>
+											<li>
+												<a href="">Paris</a>
+											</li>
+											<li>
+												<a href="">Marseille</a>
+											</li>
+											<li>
+												<a href="">Bordeaux</a>
+											</li>
+											<li>
+												<a href="">Brest</a>
+											</li>
+											<li>
+												<a href="">Reims</a>
+											</li>
+
+										</ul>
+									</div>
+							</ul>
+							</nav>
+							
+						</div><!-- /.navbar-collapse -->
+					</div><!-- /.container-fluid -->
+				</div>
+
+			</header>
+			
